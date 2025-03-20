ba57957ba7c4909bdf883241419d756c72b7fce2
Just-Xt <james.2947@hotmail.com>
2025-03-19 22:44:33 -0400

    Remove unused stuff

--- keyboards/keebio/iris_ce/keymaps/Just-Xt/combo.c
+++ keyboards/keebio/iris_ce/keymaps/Just-Xt/combo.c
@@ -17,11 +17,6 @@ void install_combo_entries(void) {
         
     vial_combo_entry_t dfjk_caps =
         {{ KC_D, KC_F, KC_J, KC_K }, KC_CAPS };
-        
-    vial_combo_entry_t set_gaming_layer = 
-        {{ KC_F13, KC_F14, KC_NO, KC_NO }, TO(4) };
-    vial_combo_entry_t set_base_layer = 
-        {{ KC_F15, KC_F16, KC_NO, KC_NO }, TO(0) };
     
     dynamic_keymap_set_combo(0, &lalt);
     dynamic_keymap_set_combo(1, &lctrl);
@@ -32,9 +27,6 @@ void install_combo_entries(void) {
     dynamic_keymap_set_combo(5, &ralt);
     
     dynamic_keymap_set_combo(6, &dfjk_caps);
-    
-    dynamic_keymap_set_combo(7, &set_gaming_layer);
-    dynamic_keymap_set_combo(8, &set_base_layer);
 }
 
 
--- keyboards/keebio/iris_ce/keymaps/Just-Xt/tapdance.c
+++ keyboards/keebio/iris_ce/keymaps/Just-Xt/tapdance.c
@@ -2,17 +2,14 @@
 
 #define TAP_TAPPING_TERM 200
 void install_tap_dance_entries(void) {
-   
-    vial_tap_dance_entry_t td0 = 
-      { KC_SPC, KC_RCTL, KC_NO, KC_NO, 215 };
+    
                                    
     vial_tap_dance_entry_t td1 = 
       { KC_LBRC, KC_RBRC, KC_NO, KC_NO, TAP_TAPPING_TERM };
       
     vial_tap_dance_entry_t td2 = 
       { KC_2, RALT(KC_2), KC_NO, KC_NO, TAP_TAPPING_TERM };
-      
-    dynamic_keymap_set_tap_dance(0, &td0);
+       
     dynamic_keymap_set_tap_dance(1, &td1);
     dynamic_keymap_set_tap_dance(2, &td2);
 }
